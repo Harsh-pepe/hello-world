6ea8a01fb9b0aa62d3f478eda2187a6d09743abc
remotlee <61445903+Harsh-pepe@users.noreply.github.com>
2024-06-12 10:49:43 +0530

    Create main.tf

---
+++ ec2/main.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "this" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  root_block_device {
+    volume_size = var.volume_size
+  }
+
+  tags = {
+    Name = "main-instance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.this.id
+}
+
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
