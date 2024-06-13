244f477dc8512f5ff89f830383c012cb5eaa9771
remotlee <61445903+Harsh-pepe@users.noreply.github.com>
2024-06-13 10:23:56 +0530

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.this.id
 }
+
+output "private_subnet_id" {
+  description = "The ID of the private subnet"
+  value       = aws_subnet.private.id
+}
