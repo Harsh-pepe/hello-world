4f485195327675c7e447fc2f727834094e924ac9
remotlee <61445903+Harsh-pepe@users.noreply.github.com>
2024-06-13 10:27:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,13 +7,10 @@ provider "aws" {
 module "vpc" {
   source       = "./vpc"
   vpc_cidr     = "10.0.0.0/16"
-}
-
-module "private_subnet" {
-  source       = "./subnet"
-  vpc_id       = module.vpc.vpc_id
   private_subnet_cidr  = "10.0.1.0/24"
 }
+
+
 
 module "ec2_instance" {
   source          = "./ec2"
