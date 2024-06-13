329ed11dd31289cb17a4528ab20e7acdce83a9e1
remotlee <61445903+Harsh-pepe@users.noreply.github.com>
2024-06-13 10:24:13 +0530

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -2,3 +2,8 @@ variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
 }
+
+variable "private_subnet_cidr" {
+  description = "The CIDR block for the private subnet"
+  type        = string
+}
